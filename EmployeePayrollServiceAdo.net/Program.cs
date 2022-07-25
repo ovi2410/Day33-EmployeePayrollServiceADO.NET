93516d8a728dc7f08c06e11c2c3f1dbdea1798ef
ovi2410 <guneshwarifegade@gmail.com>
2022-07-25 16:18:16 +0530

    [ovi]Add:UC1 database connection added

---
+++ EmployeePayrollServiceAdo.net/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+namespace EmployeePayRollService_ADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------Welcome TO Employee PayRoll Services Using ADO.NET");
+
+        }
+    }
+}
